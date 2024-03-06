819595d58a50ac1f80ed406ac2ecbff6db7277aa
dheeraj sai Gogineni <39270552+gogineni1998@users.noreply.github.com>
2024-03-06 18:57:26 -0500

    added basic program

---
+++ basic_usecase/go.mod
@@ -0,0 +1,3 @@
+module github.com/gogineni1998/go_concurency/tree/master/basic_usecase
+
+go 1.22.0
--- basic_usecase/main.go
+++ basic_usecase/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+
+	defer func() {
+		fmt.Println(time.Since(start))
+	}()
+
+	evilNinjas := []string{"Tommy", "Johnny", "Bobby", "Andy"}
+
+	for _, evilevilNinja := range evilNinjas {
+		go attack(evilevilNinja)
+	}
+	time.Sleep(time.Second * 1)
+}
+
+func attack(trojon string) {
+	fmt.Println("The ninjas are handeling trojen : ", trojon)
+	time.Sleep(time.Second * 1)
+}

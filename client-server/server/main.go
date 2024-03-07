8e7d7fa3a774aca777b64dba45e0e436e9f3ff49
dheeraj sai Gogineni <39270552+gogineni1998@users.noreply.github.com>
2024-03-07 13:17:42 -0500

    2nd module

--- client-server/server/main.go
+++ client-server/server/main.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go handelconn(conn)
+	}
+}
+
+func handelconn(conn net.Conn) {
+	defer conn.Close()
+	for {
+		res, err := conn.Write([]byte("hi form sever\n"))
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(res)
+	}
+
+}

141f69fc06ddfcdcc8f0fe9e85d394e1df3a09e0
dheeraj sai Gogineni <39270552+gogineni1998@users.noreply.github.com>
2024-03-07 14:05:43 -0500

    added new module

--- closure/main.go
+++ closure/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+func main() {
+	var wg sync.WaitGroup
+
+	inc := func(wg *sync.WaitGroup) {
+		var i int
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			i++
+			fmt.Println(i)
+		}()
+	}
+	inc(&wg)
+	wg.Wait()
 }

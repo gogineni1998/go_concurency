3c3a5c876f01f365e90b931b28abf6c7220056df
dheeraj sai Gogineni <39270552+gogineni1998@users.noreply.github.com>
2024-03-07 17:07:45 -0500

    added channels

---
+++ closure-2/go.mod
@@ -0,0 +1,3 @@
+module github.com/gogineni1998/go_concurency/tree/master/closure-2
+
+go 1.22.0
--- closure-2/main.go
+++ closure-2/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	i := 1
+	for i = 1; i <= 3; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			//time.Sleep(time.Second * 3)
+			fmt.Println(i)
+		}(i)
+	}
+	fmt.Println(i)
+	wg.Wait()
+}

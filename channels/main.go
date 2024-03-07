c05d84310833acf62e190889252e5fb8f95ee365
dheeraj sai Gogineni <39270552+gogineni1998@users.noreply.github.com>
2024-03-07 17:27:13 -0500

    Update main.go

--- channels/main.go
+++ channels/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	ch := make(chan bool, 2)
+
+	go func() {
+		defer close(ch)
+		for i := 1; i <= 5; i++ {
+			ch <- true
+			fmt.Println("sending into channel")
+		}
+	}()
+
+	for value := range ch {
+		fmt.Println("receiving from channel", value)
+	}
 }

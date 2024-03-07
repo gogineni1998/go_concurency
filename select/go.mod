0502031ea34a25247380448c10b5c0385de871ce
dheeraj sai Gogineni <39270552+gogineni1998@users.noreply.github.com>
2024-03-07 18:55:02 -0500

    select

---
+++ select/go.mod
@@ -0,0 +1,3 @@
+module github.com/gogineni1998/go_concurency/tree/master/select
+
+go 1.22.1
--- select/main.go
+++ select/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	go func() {
+		ch1 <- 1
+	}()
+
+	go func() {
+		ch2 <- 2
+	}()
+
+	for i := 0; i < 2; i++ {
+		select {
+		case a := <-ch1:
+			fmt.Println(a)
+
+		case b := <-ch2:
+			fmt.Println(b)
+		}
+	}
 }

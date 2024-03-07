5fde900ff534f50e9a134f08d79427271e745e00
dheeraj sai Gogineni <39270552+gogineni1998@users.noreply.github.com>
2024-03-07 13:42:13 -0500

    addedd new module

--- fork-join/main.go
+++ fork-join/main.go
@@ -1,5 +1,19 @@
 package main
 
-func ()  {
-	
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	count := 0
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		count++
+	}()
+
+	wg.Wait()
+	fmt.Println(count)
 }

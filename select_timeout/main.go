e60635084db322024324d426a96a3a34551dab23
dheeraj sai Gogineni <39270552+gogineni1998@users.noreply.github.com>
2024-03-07 19:21:58 -0500

    updated code

--- select_timeout/main.go
+++ select_timeout/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	ch := make(chan string, 1)
+
+	go func() {
+		time.Sleep(time.Second)
+		ch <- "one"
+	}()
+
+	select {
+	case m := <-ch:
+		fmt.Println(m)
+
+	case <-time.After(time.Microsecond * 100000000):
+		fmt.Println("timmed out")
+	}
 }

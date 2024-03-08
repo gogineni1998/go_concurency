d94b05c8230b8b12c5866365723ef3b18501fdbd
dheeraj sai Gogineni <39270552+gogineni1998@users.noreply.github.com>
2024-03-08 17:41:50 -0500

    sync package

---
+++ sync_go/go.mod
@@ -0,0 +1,3 @@
+module github.com/gogineni1998/go_concurency/tree/master/sync_go
+
+go 1.22.1
--- sync_go/main.go
+++ sync_go/main.go
@@ -1,5 +1,56 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
 
+func main() {
+	balanceCheck()
+	balanceCheckAtomic()
+}
+
+func balanceCheck() {
+	mx := sync.Mutex{}
+	wg := sync.WaitGroup{}
+	balance := 0
+
+	for i := 0; i < 100; i++ {
+		wg.Add(2)
+		go func() {
+			defer wg.Done()
+			mx.Lock()
+			balance = balance + 1
+			mx.Unlock()
+		}()
+		go func() {
+			defer wg.Done()
+			mx.Lock()
+			balance = balance - 1
+			mx.Unlock()
+		}()
+	}
+	wg.Wait()
+	fmt.Println(balance)
+}
+
+func balanceCheckAtomic() {
+	wg := sync.WaitGroup{}
+
+	var balance int64
+
+	for i := 0; i < 100; i++ {
+		wg.Add(2)
+		go func() {
+			defer wg.Done()
+			atomic.AddInt64(&balance, 1)
+		}()
+		go func() {
+			defer wg.Done()
+			atomic.AddInt64(&balance, -1)
+		}()
+	}
+	wg.Wait()
+	fmt.Println(balance)
 }

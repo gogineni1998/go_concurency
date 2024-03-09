7ef86a34154c5b436a1ae013a0d7c632357d1487
dheeraj sai Gogineni <39270552+gogineni1998@users.noreply.github.com>
2024-03-08 19:10:38 -0500

    done for the day

---
+++ signal_wait/go.mod
@@ -0,0 +1,3 @@
+module github.com/gogineni1998/go_concurency/tree/master/signal_wait
+
+go 1.22.1
--- signal_wait/main.go
+++ signal_wait/main.go
@@ -1,1 +1,54 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	mx := sync.Mutex{}
+	c := sync.NewCond(&mx)
+	wg := sync.WaitGroup{}
+
+	m := make(map[int]string)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		c.L.Lock()
+		_, ok := m[0]
+		if !ok {
+			c.Wait()
+			fmt.Println(0)
+		}
+		fmt.Println(m[0])
+		c.L.Unlock()
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		c.L.Lock()
+		_, ok := m[1]
+		if !ok {
+			c.Wait()
+		}
+		fmt.Println(m[1])
+		c.L.Unlock()
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		c.L.Lock()
+		m[0] = "dheeraj"
+		m[1] = "satya"
+		c.L.Unlock()
+		c.Signal()
+		fmt.Println(2)
+		time.Sleep(time.Second * 5)
+		c.Signal()
+	}()
+	wg.Wait()
+}

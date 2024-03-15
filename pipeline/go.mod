0dd691fd371f79178742bbe71c60f058746bd7d1
dheeraj sai Gogineni <39270552+gogineni1998@users.noreply.github.com>
2024-03-15 18:33:46 -0400

    updated new code

---
+++ pipeline/go.mod
@@ -0,0 +1,3 @@
+module github.com/gogineni1998/go_concurency/tree/master/pipeline
+
+go 1.22.1
--- pipeline/main.go
+++ pipeline/main.go
@@ -1,18 +1,31 @@
 package main
 
+import "fmt"
+
 // TODO: Build a Pipeline
 // generator() -> square() -> print
 
 // generator - convertes a list of integers to a channel
-func generator(nums ...int) {
+func generator(nums ...int) <-chan int {
+	out := make(chan int, 6)
+	defer close(out)
+	for _, num := range nums {
+		out <- num
+	}
 
+	return out
 }
 
 // square - receive on inbound channel
 // square the number
 // output on outbound channel
-func square() {
-
+func square(in <-chan int) <-chan int {
+	out := make(chan int, 6)
+	defer close(out)
+	for n := range in {
+		out <- n * n
+	}
+	return out
 }
 
 func main() {
@@ -21,5 +34,7 @@ func main() {
 	// run the last stage of pipeline
 	// receive the values from square stage
 	// print each one, until channel is closed.
-
+	for val := range square(square(generator(1, 2, 3, 4, 5))) {
+		fmt.Println(val)
+	}
 }

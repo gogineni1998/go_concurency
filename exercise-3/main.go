0de66151a1c8eb3b230c5b50f082503f86de801c
dheeraj sai Gogineni <39270552+gogineni1998@users.noreply.github.com>
2024-03-16 15:15:43 -0400

    added new exercise

--- exercise-3/main.go
+++ exercise-3/main.go
@@ -2,9 +2,13 @@
 
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
 func generator(nums ...int) <-chan int {
 	out := make(chan int)
-
 	go func() {
 		for _, n := range nums {
 			out <- n
@@ -14,25 +18,51 @@ func generator(nums ...int) <-chan int {
 	return out
 }
 
-func square(in <-chan int) <-chan int {
-	out := make(chan int)
+func square(in <-chan int) (<-chan int, <-chan int) {
+	out1 := make(chan int)
+	out2 := make(chan int)
 	go func() {
 		for n := range in {
-			out <- n * n
+			out1 <- n * n
 		}
-		close(out)
+		close(out1)
 	}()
-	return out
+
+	go func() {
+		defer close(out2)
+		for n := range in {
+			out2 <- n * n
+		}
+	}()
+	return out1, out2
 }
 
 func merge(cs ...<-chan int) <-chan int {
 	// Implement fan-in
 	// merge a list of channels to a single channel
+	out := make(chan int)
+	wg := sync.WaitGroup{}
+	for _, ch := range cs {
+		wg.Add(1)
+		go func() {
+			for val := range ch {
+				out <- val
+			}
+			wg.Done()
+		}()
+	}
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
 }
 
 func main() {
-	in := generator(2, 3)
-
+	in := generator(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 34, 5678)
+	for val := range merge(square(in)) {
+		fmt.Println(val)
+	}
 	// TODO: fan out square stage to run two instances.
 
 	// TODO: fan in the results of square stages.
